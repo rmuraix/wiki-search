1a89b2ee734dcad59d7bd862598796b2c8109418
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-07-07 17:47:26 +0900

    Format code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './style.css';
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <h1>Wiki Search</h1>
     <div class="wikipedia__search">
@@ -14,15 +14,18 @@ document.querySelector('#app').innerHTML = `
   </div>
 `;
 (() => {
- 
   const wikiInput = document.getElementById("wikipedia__search--input"); //input部分
   const wikiButton = document.getElementById("wikipedia__search--button"); //button部分
   const wikiBody = document.getElementById("card"); //表示させるエリア
 
-  const wikiFetch = async (inputValue) => { //asyncで非同期処理だと宣言する
-    const fetchValue = fetch(`https://ja.wikipedia.org/w/api.php?format=json&action=query&origin=*&list=search&srlimit=45&srsearch=${inputValue}`, {
-      method: "GET"
-    })
+  const wikiFetch = async (inputValue) => {
+    //asyncで非同期処理だと宣言する
+    const fetchValue = fetch(
+      `https://ja.wikipedia.org/w/api.php?format=json&action=query&origin=*&list=search&srlimit=45&srsearch=${inputValue}`,
+      {
+        method: "GET",
+      },
+    )
       .then((value) => {
         return value.json(); //wikipediaからのデータをJSON形式に変換
       })
@@ -35,17 +38,20 @@ document.querySelector('#app').innerHTML = `
     const element = document.getElementById("card");
 
     if (!valueTargets.length) {
-      element.insertAdjacentHTML('beforeend',
+      element.insertAdjacentHTML(
+        "beforeend",
         `
           <p>検索したワードはヒットしませんでした。</p>
-          `);
+          `,
+      );
     } else {
       for (const elem of valueTargets) {
         const elemDate = elem.timestamp;
-        const snippet = elem.snippet.slice(0,200);
+        const snippet = elem.snippet.slice(0, 200);
         const elemDateSlice = elemDate.slice(0, 10).replace(/-/g, ".");
-        element.insertAdjacentHTML('beforeend',
-        `
+        element.insertAdjacentHTML(
+          "beforeend",
+          `
           <div class="wikicard">
             <a href="http://jp.wikipedia.org/?curid=${elem.pageid}" target="_blank">
             <h2 class="wikicard__title">${elem.title}
@@ -57,13 +63,12 @@ document.querySelector('#app').innerHTML = `
             </a>
             
         </div>
-          `
+          `,
         );
-      
       }
     }
   };
-  
+
   //クリックイベントに設定している関数
   const wikiData = () => {
     wikiBody.innerHTML = ""; //一旦js-wikipedia-bodyの中を空にする

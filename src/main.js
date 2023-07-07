99f95494ef8c57c103b13a7f9ec66caf463d359e
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-07-07 12:10:20 +0900

    Add snipet

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Wiki Serch</h1>
-    
+    <h1>Wiki Search</h1>
     <div class="wikipedia__search">
-      <input id="wikipedia__serch--input" type="text" value="" placeholder="検索ワード">
-      <button id="wikipedia__serch--button" type="button">検索</button>
+      <input id="wikipedia__search--input" type="text" value="" placeholder="検索ワード">
+      <button id="wikipedia__search--button" type="button">検索</button>
     </div>
     <div id="card">
       <div class="wikipedia__results></div>
@@ -16,8 +15,8 @@ document.querySelector('#app').innerHTML = `
 `;
 (() => {
  
-  const wikiInput = document.getElementById("wikipedia__serch--input"); //input部分
-  const wikiButton = document.getElementById("wikipedia__serch--button"); //button部分
+  const wikiInput = document.getElementById("wikipedia__search--input"); //input部分
+  const wikiButton = document.getElementById("wikipedia__search--button"); //button部分
   const wikiBody = document.getElementById("card"); //表示させるエリア
 
   const wikiFetch = async (inputValue) => { //asyncで非同期処理だと宣言する
@@ -33,23 +32,25 @@ document.querySelector('#app').innerHTML = `
 
     const valueJson = await fetchValue; //非同期処理を実行
     const valueTargets = valueJson.query.search; //必要な情報が入っている配列を取得
+    const element = document.getElementById("card");
 
     if (!valueTargets.length) {
-      const wikiNull = document.createElement("p");
-      wikiNull.classList.add("p-wikipedia__null");
-      wikiNull.textContent = "検索したワードはヒットしませんでした。";
-      wikiBody.appendChild(wikiNull);
+      element.insertAdjacentHTML('beforeend',
+        `
+          <p>検索したワードはヒットしませんでした。</p>
+          `);
     } else {
-      const element = document.getElementById("card");
       for (const elem of valueTargets) {
         const elemDate = elem.timestamp;
+        const snippet = elem.snippet.slice(0,200);
         const elemDateSlice = elemDate.slice(0, 10).replace(/-/g, ".");
         element.insertAdjacentHTML('beforeend',
         `
           <div class="wikicard">
             <a href="http://jp.wikipedia.org/?curid=${elem.pageid}" target="_blank">
-            <h3 class="wikicard__title">${elem.title}
-            </h3>
+            <h2 class="wikicard__title">${elem.title}
+            </h2>
+            <p>${snippet}...</p>
             <div class="wikicard__date">
             最終更新日：${elemDateSlice}
             </div>
